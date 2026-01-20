455821279ec92b6489a16e7447f27c21fd782756
Nimbus <210294428+Nimbus-commints@users.noreply.github.com>
2026-01-20 16:57:03 -0500

    added DRF serialization

--- Reservation/models.py
+++ Reservation/models.py
@@ -6,7 +6,17 @@ from django.db import models
 class Booking(models.Model):
     nombre = models.CharField(max_length=255)
     dia_de_reserva = models.DateField()
-    n_personas = models.SmallIntegerField(default=10)
+    n_de_personas = models.SmallIntegerField(default=10)
+    fecha_de_reserva = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.first_name
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.title
